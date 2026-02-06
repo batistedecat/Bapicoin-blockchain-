69644610656735fb390911c255d907021f279376
batistedecat <batiste.decat@icloud.com>
2026-02-06 21:29:40 +0100

    commented out unused code

--- bapicoin.py
+++ bapicoin.py
@@ -200,6 +200,6 @@ def concensus():
 
 
 
-#run server on port 5000
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port = 5001)
+# #run server on port 5000
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port = 5001)
